1b484a4137af661d6a963278135e042de55a0fb4
YULIAN CHRISTOFER ORTIZ MORALES <yortizm1@miumg.edu.gt>
2021-04-10 20:40:15 -0600

    Prueba Edicion 1

--- Prueba_Arquitectura/Form1.Designer.vb
+++ Prueba_Arquitectura/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frm_main
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,74 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.btn_inve = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.LightGray
+        Me.Panel1.Controls.Add(Me.Panel3)
+        Me.Panel1.Controls.Add(Me.btn_inve)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(233, 500)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.DimGray
+        Me.Panel3.Location = New System.Drawing.Point(0, 99)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(10, 42)
+        Me.Panel3.TabIndex = 2
+        '
+        'btn_inve
+        '
+        Me.btn_inve.FlatAppearance.BorderSize = 0
+        Me.btn_inve.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_inve.Font = New System.Drawing.Font("Comic Sans MS", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_inve.ForeColor = System.Drawing.Color.Black
+        Me.btn_inve.Image = Global.Prueba_Arquitectura.My.Resources.Resources.inventario
+        Me.btn_inve.Location = New System.Drawing.Point(0, 99)
+        Me.btn_inve.Name = "btn_inve"
+        Me.btn_inve.Size = New System.Drawing.Size(233, 42)
+        Me.btn_inve.TabIndex = 0
+        Me.btn_inve.Text = "    Inventario"
+        Me.btn_inve.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btn_inve.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Gainsboro
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(233, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(607, 33)
+        Me.Panel2.TabIndex = 1
+        '
+        'frm_main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(840, 500)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frm_main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents btn_inve As Button
+    Friend WithEvents Panel2 As Panel
 End Class
--- Prueba_Arquitectura/Form1.vb
+++ Prueba_Arquitectura/Form1.vb
@@ -1,3 +1,9 @@
-﻿Public Class Form1
+﻿Public Class frm_main
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
 
+    End Sub
+
+    Private Sub btn_inve_Click(sender As Object, e As EventArgs) Handles btn_inve.Click
+
+    End Sub
 End Class
